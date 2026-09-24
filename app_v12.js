9791fbfc2186a2138df6f174e50abd89542db7d4
Admin <admin@eco-bem>
2026-09-24 08:44:44 +0100

    Update API_URL to use Vercel for file:// origin

--- app_v12.js
+++ app_v12.js
@@ -1593,7 +1593,7 @@ window.updateUserUI = function() {
 
 
 /* ══════════════ AUTH & DB SYNC LOGIC ══════════════ */
-const API_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || window.location.protocol === 'file:') ? 'http://localhost:3000/api' : '/api';
+const API_URL = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') ? 'http://localhost:3000/api' : (window.location.protocol === 'file:') ? 'https://eco-bem.vercel.app/api' : '/api';
 
 window.switchAuth = function(tab) {
   document.getElementById('tab-login').classList.remove('on');
